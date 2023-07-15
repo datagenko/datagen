99765dfc6206bec009be7557c985d4cb4ae64902
Astinaus <slaayers0@gmail.com>
2023-07-15 16:23:32 +0900

    Design: 드롭다운 셀렉트박스 스타일

---
+++ asset/js/select.js
@@ -0,0 +1,20 @@
+const selectBox = document.querySelector('.select-wrap');
+const selectList = document.querySelector('#select-box');
+const option = document.querySelectorAll('.option');
+const selected = document.querySelector('.selected-value');
+
+selectBox.addEventListener('click', () => {
+  selectList.classList.toggle('active');
+});
+
+option.forEach((optionElement) => {
+  optionElement.addEventListener('click', () => {
+    selectOption(optionElement);
+  });
+});
+
+function selectOption(optionElement) {
+  const selectBox = optionElement.closest('.select-wrap');
+  const selectedElement = selectBox.querySelector('.selected-value');
+  selectedElement.textContent = optionElement.textContent;
+}
