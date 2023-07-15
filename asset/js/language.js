ca795dbcf7ddf6f96f3a9468c53ca0c119eb7a65
haron <haronxlee@gmail.com>
2023-07-15 16:58:04 +0900

    Design: language select box arrow animation 추가

--- asset/js/language.js
+++ asset/js/language.js
@@ -3,10 +3,17 @@ const languageList = document.querySelector('.language-list');
 
 languageBtn.addEventListener('click', (e) => {
   languageList.classList.toggle('on');
+  if (languageList.classList.contains('on')) {
+    languageBtn.classList.add('on');
+  } else {
+    languageBtn.classList.remove('on');
+  }
 });
+
 languageList.addEventListener('click', (e) => {
   if (e.target.nodeName === 'BUTTON') {
     languageBtn.textContent = e.target.textContent;
     languageList.classList.remove('on');
+    languageBtn.classList.remove('on');
   }
 });
