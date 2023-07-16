2ae52191067bc964192e0209ea88e10ac3d43fb2
zkdk1241 <zkdk1241@naver.com>
2023-07-16 13:06:32 +0900

    Feat: Menubar 수정

--- asset/js/menubar.js
+++ asset/js/menubar.js
@@ -1,6 +1,7 @@
 const openModalBtn = document.getElementById('openModalBtn');
 const closeModalBtn = document.getElementById('closeModalBtn');
 const bottomSheetModal = document.getElementById('bottomSheetModal');
+const modalContent = document.querySelector('.modal-content');
 
 openModalBtn.addEventListener('click', () => {
     bottomSheetModal.style.height = '100%';
@@ -9,3 +10,23 @@ openModalBtn.addEventListener('click', () => {
   closeModalBtn.addEventListener('click', () => {
     bottomSheetModal.style.height = '0';
   });
+  
+  function checkScroll() {
+    const contentHeight = modalContent.clientHeight;
+    const scrollHeight = modalContent.scrollHeight;
+  
+    if (scrollHeight > contentHeight) {
+      modalContent.style.overflowY = 'auto';
+    } else {
+      modalContent.style.overflowY = 'visible';
+    }
+  }
+
+  // 모달 내용 변경 시 호출
+function updateModalContent(newContent) {
+    modalContent.innerHTML = newContent;
+    checkScroll();
+  }
+  
+  // 초기 모달 내용 확인
+  checkScroll();
