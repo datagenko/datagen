3eb380e7d5bc46668453c8fcf74888fa288719bd
kimbareum <right.kim93@gmail.com>
2023-07-16 21:51:09 +0900

    fix: indent 수정2

--- asset/js/output.js
+++ asset/js/output.js
@@ -79,93 +79,92 @@ function money(min, max, symbol) {
 
 
 function generateData(template, index) {
-          let data = {};
-          
-          for (const i in template){
-            data[i] = template[i].replace(/<([^<>]+)>/g, (str, key) => {
-              const func = key.split('(')[0];
-              let action = "";
-              if (func === "function"){
-                action = key.match(/{(.*)}/)[1].trim();
-              }
-              [...args] = key.split('(')[1]
-                                  .replace(')', '')
-                                  .replaceAll(' ', '')
-                                  .replaceAll('\'', '')
-                                  .replaceAll('"', '')
-                                  .split(',')
+  let data = {};
+  
+  for (const i in template){
+    data[i] = template[i].replace(/<([^<>]+)>/g, (str, key) => {
+      const func = key.split('(')[0];
+      let action = "";
+      if (func === "function"){
+        action = key.match(/{(.*)}/)[1].trim();
+      }
+      [...args] = key.split('(')[1]
+                          .replace(')', '')
+                          .replaceAll(' ', '')
+                          .replaceAll('\'', '')
+                          .replaceAll('"', '')
+                          .split(',')
 
-              // 들어오는 인자는 args배열에 저장됩니다.
-              // args[0], args[1] 식으로 접근하시면 되고, 기본적으로 전부 String 타입이기 때문에, 데이터타입에 주의해서 다뤄주세요.
-              // optional로 인자가 들어오지 않았을때에 대한 처리도 필요합니다.
-              switch(func) {
-                // 고유값
-                  case 'uuid':
-                      return uuid();
-                  case 'index':
-                      return index
-                  //case 'username':
-                  //    return username()
-                  //case 'password':
-                  //    return password(min, max)
-                //특정 데이터타입
-                  case 'int':
-                      return randomInteger(parseInt(args[0]), parseInt(args[1]))
-                  //case 'float':
-                  //    return randomFloat(min, max, round)
-                  //case 'boolean':
-                  //    return randomBoolean()
-                  case 'random':
-                  //    return randomItem(args)
-                  //case 'lorem':
-                  //    // loream은 들어오는 인자가 전부 optional 이기 때문에 처리방법이 복잡할 것 같습니다.
-                  //    return lorem(number, unit)
-                  //case 'picture':
-                  //    return picture(width, height)
-                  //case 'color':
-                  //    return color()
-                // 개인정보관련
-                  //case 'name':
-                  //    return name()
-                  //case 'email':
-                  //    return email()
-                  //case 'phone':
-                  //    return phone()
-                  //case 'country':
-                  //    return country()
-                  //case 'city':
-                  //    return city()
-                  //case 'address':
-                  //    return address()
-                  //case 'postal_code':
-                  //    return postal_code()
-                  //case 'job':
-                  //    return job()
-                  //case 'company':
-                  //    return company()
-                  //case 'creditCardNumber':
-                  //    return creditCardNumber()
-                  //case 'gender':
-                  //    return gender()
-                  //case 'urls':
-                  //    return urls()
-                  case 'money':
-                      return money(parseInt(args[0]), parseInt(args[1]), args[2])
-                  //case 'date':
-                  //    return date(start, end, format)
-                  //case 'time':
-                  //    return time()
-                  //case 'function':
-                  //    const do_action = new Function(action);
-                  //    return do_action.call(data)
-                  default:
-                      return str
-              }
-            }
-            )
-          }
-          return data
-        }
+      // 들어오는 인자는 args배열에 저장됩니다.
+      // args[0], args[1] 식으로 접근하시면 되고, 기본적으로 전부 String 타입이기 때문에, 데이터타입에 주의해서 다뤄주세요.
+      // optional로 인자가 들어오지 않았을때에 대한 처리도 필요합니다.
+      switch(func) {
+        // 고유값
+          case 'uuid':
+              return uuid();
+          case 'index':
+              return index
+          //case 'username':
+          //    return username()
+          //case 'password':
+          //    return password(min, max)
+        //특정 데이터타입
+          case 'int':
+              return randomInteger(parseInt(args[0]), parseInt(args[1]))
+          //case 'float':
+          //    return randomFloat(min, max, round)
+          //case 'boolean':
+          //    return randomBoolean()
+          case 'random':
+          //    return randomItem(args)
+          //case 'lorem':
+          //    // loream은 들어오는 인자가 전부 optional 이기 때문에 처리방법이 복잡할 것 같습니다.
+          //    return lorem(number, unit)
+          //case 'picture':
+          //    return picture(width, height)
+          //case 'color':
+          //    return color()
+        // 개인정보관련
+          //case 'name':
+          //    return name()
+          //case 'email':
+          //    return email()
+          //case 'phone':
+          //    return phone()
+          //case 'country':
+          //    return country()
+          //case 'city':
+          //    return city()
+          //case 'address':
+          //    return address()
+          //case 'postal_code':
+          //    return postal_code()
+          //case 'job':
+          //    return job()
+          //case 'company':
+          //    return company()
+          //case 'creditCardNumber':
+          //    return creditCardNumber()
+          //case 'gender':
+          //    return gender()
+          //case 'urls':
+          //    return urls()
+          case 'money':
+              return money(parseInt(args[0]), parseInt(args[1]), args[2])
+          //case 'date':
+          //    return date(start, end, format)
+          //case 'time':
+          //    return time()
+          //case 'function':
+          //    const do_action = new Function(action);
+          //    return do_action.call(data)
+          default:
+              return str
+      }
+    })
+  }
+  return data
+}
 
 const input_form = document.querySelector('#json-input')
 document.getElementById('generate-button').addEventListener('click', function() {
