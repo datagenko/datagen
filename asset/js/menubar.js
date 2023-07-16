0c5a79ca591c88d1e90b21ee3eae537e9205ee16
zkdk1241 <zkdk1241@naver.com>
2023-07-16 19:49:14 +0900

    Feat: 메뉴바 수정

--- asset/js/menubar.js
+++ asset/js/menubar.js
@@ -11,14 +11,17 @@ closeModalBtn.addEventListener('click', () => {
   bottomSheetModal.style.height = '0';
 });
 
+// 모달 창의 내용 높이와 스크롤 높이를 비교하여 스크롤이 필요한지 여부를 결정
 function checkScroll() {
+    // 모달 창 내용의 실제 높이
   const contentHeight = modalContent.clientHeight;
+    //모달 창의 콘텐츠가 모두 표시될 때 필요한 높이
   const scrollHeight = modalContent.scrollHeight;
 
   if (scrollHeight > contentHeight) {
-    modalContent.style.overflowY = 'auto';
+    modalContent.style.overflowY = 'auto';  // 세로 스크롤 활성화
   } else {
-    modalContent.style.overflowY = 'visible';
+    modalContent.style.overflowY = 'visible';  // 세로 스크롤 비활성화
   }
 }
 
