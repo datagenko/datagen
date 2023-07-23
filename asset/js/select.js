aeadfca5a1b4b056ee084da415296493a8dd741a
goyomi <ghdl1004797@gmail.com>
2023-07-23 23:59:01 +0900

    Design, Remove, Refactor  Design: select box 공통 css, output 간격수정  Remove: language.js 삭제  Refactor: 셀렉트박스 태그 및 기능 통일
    Co-authored-by: haron-lee <haronxlee@gmail.com>
    Co-authored-by: hyeri-woo <woohyeri0525@gmail.com>
    Co-authored-by: junny97 <pcohad12@gmail.com>
    Co-authored-by: sypaik-dev <sypaik.dev@gmail.com>
    Co-authored-by: zkdk1241 <yseojin1010@gmail.com>
    Co-authored-by: yonainthefish <yonainthefish@gmail.com>
    Co-authored-by: D-Sup <andongcelsius@gmail.com>
    #1, #11

--- asset/js/select.js
+++ asset/js/select.js
@@ -1,30 +1,43 @@
-const selectBox = document.querySelector('#datagen-input .select-wrap');
-const option = document.querySelectorAll('#datagen-input .select-wrap li');
-const selectedValue = document.querySelector('#selected-value');
-
-selectBox.addEventListener('click', (event) => {
-  event.stopPropagation(); // 이벤트 전파 막기
-  selectBox.classList.toggle('active');
-});
-
-option.forEach((optionElement) => {
-  optionElement.addEventListener('click', (event) => {
+const selectEvent = (selectBox, option, selectedValue) => {
+  selectBox.addEventListener('click', (event) => {
     event.stopPropagation(); // 이벤트 전파 막기
-    selectOption(optionElement);
+    selectBox.classList.toggle('active');
   });
-});
 
-function selectOption(optionElement) {
-  const selectedText = optionElement.querySelector('a').textContent;
-  selectedValue.textContent = selectedText;
-  selectBox.classList.remove('active');
-}
+  option.forEach((optionElement) => {
+    optionElement.addEventListener('click', (event) => {
+      event.stopPropagation(); // 이벤트 전파 막기
+      selectOption(optionElement);
+    });
+  });
 
-document.addEventListener('click', (event) => {
-  const target = event.target;
-  const selectWrap = document.querySelector('#datagen-input .select-wrap');
-
-  if (!selectWrap.contains(target)) {
+  function selectOption(optionElement) {
+    const selectedText = optionElement.querySelector('button').textContent;
+    selectedValue.textContent = selectedText;
     selectBox.classList.remove('active');
   }
-});
+
+  document.addEventListener('click', (event) => {
+    const target = event.target;
+    const selectWrap = document.querySelector('#datagen-input .select-input');
+
+    if (!selectWrap.contains(target)) {
+      selectBox.classList.remove('active');
+    }
+  });
+}
+
+const selectBoxInput = document.querySelector('#datagen-input .select-input');
+const optionInput = document.querySelectorAll('#datagen-input .select-input li');
+const selectedValueInput = document.querySelector('.select-input .selected-value');
+selectEvent(selectBoxInput, optionInput, selectedValueInput);
+
+const selectBoxIndent = document.querySelector('.select-indent');
+const optionIndent = document.querySelectorAll('.select-indent li');
+const selectedValueIndent = document.querySelector('.select-indent .selected-value');
+selectEvent(selectBoxIndent, optionIndent, selectedValueIndent);
+
+const selectBoxLang = document.querySelector('.select-language');
+const optionLang = document.querySelectorAll('.select-language li');
+const selectedValueLang = document.querySelector('.select-language .selected-value');
+selectEvent(selectBoxLang, optionLang, selectedValueLang);
