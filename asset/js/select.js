5a81e0cc7903d2e0f780c24137f7915ac61fa4db
soooee4 <soooee4@gmail.com>
2023-07-24 21:33:05 +0900

    Feat: 데이터 추가 기능 구현

--- asset/js/select.js
+++ asset/js/select.js
@@ -1,63 +1,102 @@
 const selectEvent = (selectBox, option, selectedValue) => {
-  selectBox.addEventListener('click', (event) => {
+  selectBox.addEventListener("click", (event) => {
     event.stopPropagation(); // 이벤트 전파 막기
-    selectBox.classList.toggle('active');
+    selectBox.classList.toggle("active");
   });
 
   option.forEach((optionElement) => {
-    optionElement.addEventListener('click', (event) => {
+    optionElement.addEventListener("click", (event) => {
       event.stopPropagation(); // 이벤트 전파 막기
-      const selectionStart = defaultTemplate.value.indexOf('\n', defaultTemplate.selectionStart);
+      const selectionStart = defaultTemplate.value.indexOf(
+        "\n",
+        defaultTemplate.selectionStart
+      );
       selectOption(optionElement, selectionStart);
     });
   });
 
+  const templateMapping = {
+    "uuid: user id 생성": `"uuid": "<uuid()>",`,
+    index: `"index": "<index(integer)>",`,
+    username: `"username": "<username()>",`,
+    password: `"password": "<password(min_length, max_length)>",`,
+    int: `"int": "<int(min, max)>",`,
+    float: `"float": "<float(min, max, round)>",`,
+    boolean: `"boolean": "<boolean()>",`,
+    random: `"random": "<random("item1", "item2", "item3", …)>",`,
+    lorem: `"lorem": "<lorem(number, unit)>",`,
+    picture: `"picture": "<picture(width, height)>",`,
+    color: `"color": "<color()>",`,
+    name: `"name": "<name()>",`,
+    email: `"email": "<email()>",`,
+    phone: `"phone": "<phone()>",`,
+    country: `"country": "<country()>",`,
+    city: `"city": "<city()>",`,
+    address: `"address": "<address()>",`,
+    "postal-code": `"postal-code": "<postal-code()>",`,
+    job: `"job": "<job()>",`,
+    company: `"company": "<company()>",`,
+    creditCardNumber: `"creditCardNumber": "<creditCardNumber()>",`,
+    gender: `"gender": "<gender()>",`,
+    urls: `"urls": "<urls()>",`,
+    money: `"money": "<money(min, max, symbol)>",`,
+    date: `"date": "<date(date_start, date_end, date_format)>",`,
+    time: `"time": "<time()>",`,
+    iter: `"iter": "<iter(number)>",`,
+    function: `"function": "<function() { functionString }>",`,
+  };
+
   function selectOption(optionElement, selectionStart) {
-    let selectedText = optionElement.querySelector('button').textContent;
-    console.log(selectedText);
-    if(selectedText === "city"){
-      selectedText = `"city": "<city()>",`
+    let selectedText = optionElement.querySelector("button").textContent;
+
+    if (templateMapping[selectedText]) {
+      selectedData = templateMapping[selectedText];
     }
+
     selectedValue.textContent = selectedText;
-    selectBox.classList.remove('active');
+    selectBox.classList.remove("active");
     if (selectionStart === -1) {
-      const braceStartIndex = defaultTemplate.value.lastIndexOf('}');
+      const braceStartIndex = defaultTemplate.value.lastIndexOf("}");
       if (braceStartIndex !== -1) {
-        const commaIndex = defaultTemplate.value.lastIndexOf(',', braceStartIndex);
-        
+        const commaIndex = defaultTemplate.value.lastIndexOf(
+          ",",
+          braceStartIndex
+        );
+
         if (commaIndex !== -1) {
-          const beforeNameStr = defaultTemplate.value.slice(0, commaIndex+1);
+          const beforeNameStr = defaultTemplate.value.slice(0, commaIndex + 1);
           const afterNameStr = defaultTemplate.value.slice(commaIndex + 1);
-          const newValue = beforeNameStr + '\n' + '      ' + selectedText +  afterNameStr;
-  
+          const newValue =
+            beforeNameStr + "\n" + "      " + selectedData + afterNameStr;
+
           defaultTemplate.value = newValue;
         }
       }
-    }else{
-    const beforeNameStr = defaultTemplate.value.slice(0, selectionStart + 1);
-    const afterNameStr = defaultTemplate.value.slice(selectionStart + 1);
-    console.log(beforeNameStr);
-    console.log(afterNameStr);
-    const newValue = beforeNameStr + '      ' + selectedText + '\n' + afterNameStr;
-    console.log(newValue);
-  
-    defaultTemplate.value = newValue;
-  }
+    } else {
+      const beforeNameStr = defaultTemplate.value.slice(0, selectionStart + 1);
+      const afterNameStr = defaultTemplate.value.slice(selectionStart + 1);
+      console.log(beforeNameStr);
+      console.log(afterNameStr);
+      const newValue =
+        beforeNameStr + "      " + selectedData + "\n" + afterNameStr;
+      console.log(newValue);
+
+      defaultTemplate.value = newValue;
+    }
   }
 
-  document.addEventListener('click', (event) => {
+  document.addEventListener("click", (event) => {
     const target = event.target;
-    const selectWrap = document.querySelector('#datagen-input .select-input');
+    const selectWrap = document.querySelector("#datagen-input .select-input");
 
     if (!selectWrap.contains(target)) {
-      selectBox.classList.remove('active');
+      selectBox.classList.remove("active");
     }
   });
-}
+};
 
-const defaultTemplate = document.querySelector('#json-input');
-defaultTemplate.value = 
-`[
+const defaultTemplate = document.querySelector("#json-input");
+defaultTemplate.value = `[
   "<iter(5)>",
   {
       "_id": "<uuid()>",
@@ -72,20 +111,25 @@ defaultTemplate.value =
   }
 ]`;
 
-const selectBoxInput = document.querySelector('#datagen-input .select-input');
-const optionInput = document.querySelectorAll('#datagen-input .select-input li');
-const selectedValueInput = document.querySelector('.select-input .selected-value');
+const selectBoxInput = document.querySelector("#datagen-input .select-input");
+const optionInput = document.querySelectorAll(
+  "#datagen-input .select-input li"
+);
+const selectedValueInput = document.querySelector(
+  ".select-input .selected-value"
+);
 selectEvent(selectBoxInput, optionInput, selectedValueInput, defaultTemplate);
 
-const selectBoxIndent = document.querySelector('.select-indent');
-const optionIndent = document.querySelectorAll('.select-indent li');
-const selectedValueIndent = document.querySelector('.select-indent .selected-value');
+const selectBoxIndent = document.querySelector(".select-indent");
+const optionIndent = document.querySelectorAll(".select-indent li");
+const selectedValueIndent = document.querySelector(
+  ".select-indent .selected-value"
+);
 selectEvent(selectBoxIndent, optionIndent, selectedValueIndent);
 
-const selectBoxLang = document.querySelector('.select-language');
-const optionLang = document.querySelectorAll('.select-language li');
-const selectedValueLang = document.querySelector('.select-language .selected-value');
+const selectBoxLang = document.querySelector(".select-language");
+const optionLang = document.querySelectorAll(".select-language li");
+const selectedValueLang = document.querySelector(
+  ".select-language .selected-value"
+);
 selectEvent(selectBoxLang, optionLang, selectedValueLang);
-
-
-
