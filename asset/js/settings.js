382f1d1e6356f669ccbe67e1e42f4b98beff36aa
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-07-25 23:42:28 +0900

    Refactor: 설정 모달 로직 간편화

--- asset/js/settings.js
+++ asset/js/settings.js
@@ -1,47 +1,29 @@
-const settingBtn = document.querySelector('.btn-setting');
-const settingModal = document.querySelector('.setting-list');
-const textArea = document.querySelector('#json-output');
-const closeBtn = settingModal.querySelector('.btn-setting_close');
+const settingBtn = document.querySelector(".btn-setting");
+const settingModal = document.querySelector(".setting-list");
+const closeBtn = settingModal.querySelector(".btn-setting_close");
 
-const copyBtn = document.querySelector('.btn-copy');
-const jsonOutputText = document.querySelector('#json-output');
+const copyBtn = document.querySelector(".btn-copy");
+const jsonOutputText = document.querySelector("#json-output");
 
-settingBtn.addEventListener('click', (e) => {
-  if (
-    settingModal.classList.contains('turn_off') &&
-    textArea.classList.contains('turn_on')
-  ) {
-    settingModal.classList.remove('turn_off');
-    settingModal.classList.add('turn_on');
-    textArea.classList.remove('turn_on');
-    textArea.classList.add('turn_off');
-  } else if (
-    settingModal.classList.contains('turn_on') &&
-    textArea.classList.contains('turn_off')
-  ) {
-    settingModal.classList.remove('turn_on');
-    settingModal.classList.add('turn_off');
-    textArea.classList.remove('turn_off');
-    textArea.classList.add('turn_on');
-  }
+settingBtn.addEventListener("click", () => {
+  settingModal.classList.toggle("turn_on");
 });
 
-closeBtn.addEventListener('click', (e) => {
-  settingModal.classList.add('turn_off');
-  settingModal.classList.remove('turn_on');
-  textArea.classList.add('turn_on');
-  textArea.classList.remove('turn_off');
+closeBtn.addEventListener("click", () => {
+  settingModal.classList.remove("turn_on");
 });
 
-copyBtn.addEventListener('click', (e) => {
+});
+
+copyBtn.addEventListener("click", () => {
   const textToCopy = jsonOutputText.value;
 
   navigator.clipboard
     .writeText(textToCopy)
     .then(() => {
-      alert('클립보드에 복사하였습니다.');
+      alert("클립보드에 복사하였습니다.");
     })
     .catch((err) => {
-      console.error('복사가 되지 않았습니다.', err);
+      console.error("복사가 되지 않았습니다.", err);
     });
 });
